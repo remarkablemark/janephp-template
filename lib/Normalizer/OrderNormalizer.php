68062db1980bec1a08e701de9264ab98a572b2a5
Mark <mark@remarkablemark.org>
2026-03-30 22:42:41 -0400

    build(lib): generate library

--- lib/Normalizer/OrderNormalizer.php
+++ lib/Normalizer/OrderNormalizer.php
@@ -39,18 +39,18 @@ class OrderNormalizer implements DenormalizerInterface, NormalizerInterface, Den
 
     public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): mixed
     {
-        if (isset($data['$ref'])) {
+        $object = new \Petstore\Model\Order();
+        if (null === $data || false === \is_array($data)) {
+            return $object;
+        }
+        if (isset($data['$ref']) && !isset($data['type']) && !isset($data['properties']) && !isset($data['allOf'])) {
             return new Reference($data['$ref'], $context['document-origin']);
         }
         if (isset($data['$recursiveRef'])) {
             return new Reference($data['$recursiveRef'], $context['document-origin']);
         }
-        $object = new \Petstore\Model\Order();
         if (\array_key_exists('complete', $data) && \is_int($data['complete'])) {
             $data['complete'] = (bool) $data['complete'];
-        }
-        if (null === $data || false === \is_array($data)) {
-            return $object;
         }
         if (\array_key_exists('id', $data)) {
             $object->setId($data['id']);
